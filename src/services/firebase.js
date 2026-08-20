f127122d5c00c9ec73c8f564e683d575ff35382b
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-08-20 12:04:21 -0500

    project setup

---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_db_QBGw2GQaLGUneJySYnKgo3X82szM",
+  authDomain: "fourth-bc0b3.firebaseapp.com",
+  projectId: "fourth-bc0b3",
+  storageBucket: "fourth-bc0b3.firebasestorage.app",
+  messagingSenderId: "43485389384",
+  appId: "1:43485389384:web:5b97ecb296a20df744ae7d",
+  measurementId: "G-7NDHWPT7N9",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
