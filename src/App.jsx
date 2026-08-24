2b27022941489888ed0c52348acfd5397854f163
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-08-24 16:06:42 -0500

    Completed landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import { createContext } from "react"
 
 import SignUp from "./components/auth/SignUp";
 import LogIn from "./components/auth/LogIn";
+import Landing from "./components/auth/Landing";
 
 import RootLayout from "./layouts/RootLayout";
 import PublicLayout from "./layouts/PublicLayout";
 
 import Home from "./components/common/Home";
 
-export const AuthContext = createContext(null);
-
-export default function App(){
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<PublicLayout/>}> 
-                    <Route path="signup" element={<SignUp/>} />
-                    <Route path="login" element={<LogIn/>}/>
-                </Route>
-                <Route element={<RootLayout/>}>
-                    <Route path="/app" element={<Home/>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<LogIn />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route path="/app" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/components/auth/Landing.jsx
@@ -0,0 +1,29 @@
+import FourthLogo from "../../assets/fourthlogo.png";
+import { NavLink } from "react-router";
+
+export default function Landing() {
+  return (
+    <main className="flex flex-col justify-center items-center min-h-dvh">
+      <div>
+        <img src={FourthLogo} alt="Fourth Logo." />
+      </div>
+      <div>
+        <h1 className="text-4xl">JOIN THE FOURTH WALL</h1>
+      </div>
+      <div className="flex flex-col items-center gap-4 w-full mt-12">
+        <NavLink
+          to="/signup"
+          className="flex justify-center items-center text-center h-[50px] w-[80%] bg-[var(--accent-dark)] rounded-xl active:bg-[var(--accent-dark)]/80 active:scale-98 transform-all duration-200"
+        >
+          Create Account
+        </NavLink>
+        <NavLink
+          to="/login"
+          className="flex justify-center items-center text-center h-[50px] w-[80%] bg-[var(--accent)] rounded-xl active:bg-[var(--accent)]/80 active:scale-98 transform-all duration-200"
+        >
+          Log In
+        </NavLink>
+      </div>
+    </main>
+  );
+}
