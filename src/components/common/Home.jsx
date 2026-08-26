a02a8c16263b40cffbef83a2abc6435c0e68dd47
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-08-26 17:41:36 -0500

    created dropdown box menu

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -4,14 +4,16 @@ import { useEffect, useState, useRef } from "react";
 import { getTrendingMovies } from "../../services/tmdb.js";
 
 export default function Home() {
-  const [trendingMoviesObject, setTrendingMoviesObject] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [ recentlyWatched, setRecentlyWatched ] = useState([]);
+
   const carouselRef = useRef(null);
 
   //   Get trending movie data
   useEffect(() => {
     const fetchData = async () => {
-      const trendingMovies = await getTrendingMovies();
-      setTrendingMoviesObject(trendingMovies);
+      const trendingMoviesData = await getTrendingMovies();
+      setTrendingMovies(trendingMoviesData);
     };
     fetchData();
   }, []);
@@ -21,7 +23,7 @@ export default function Home() {
     const carouselWidth = carouselRef.current.clientWidth;
     if (carouselRef.current) {
       carouselRef.current.scrollBy({
-        left: -carouselWidth,
+        left: -(carouselWidth - 30),
         behavior: "smooth",
       });
     }
@@ -31,12 +33,12 @@ export default function Home() {
     const carouselWidth = carouselRef.current.clientWidth;
     if (carouselRef.current) {
       carouselRef.current.scrollBy({
-        left: carouselWidth,
+        left: carouselWidth - 30,
         behavior: "smooth",
       });
     }
   }
-  console.log(trendingMoviesObject);
+  console.log(trendingMovies);
 
   return (
     <main className="px-4 min-h-dvh">
@@ -48,41 +50,60 @@ export default function Home() {
         />
         <Search className="absolute -translate-y-1/2 top-[50%] right-[12px]" />
       </div>
-
-      <div className="flex flex-col gap-4">
-        <div>
-          <p className="text-lg font-bold">Trending Right Now</p>
-          <hr className="w-[75%]" />
-        </div>
-        <div className="relative">
-          <button
-            onClick={scrollLeft}
-            className="absolute hidden carouselArrow top-0 bottom-0 left-0 z-10 bg-transparent hover:bg-black/50 transition-colors duration-200 cursor-pointer"
-          >
-            <ChevronLeft />
-          </button>
-          <div
-            className="flex gap-3 overflow-x-auto no-scrollbar snap-x snap-mandatory"
-            ref={carouselRef}
-          >
-            {trendingMoviesObject.map((movie) => {
-              return (
-                <div key={movie.id} className="shrink-0 snap-start">
-                  <img
-                    src={movie.poster}
-                    alt={movie.title}
-                    className="w-[9rem] md:w-[10rem] lg:w-[11rem] aspect-[2/3] border border-white/15 object-cover"
-                  />
-                </div>
-              );
-            })}
+      <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
+            <div>
+                <p className="text-lg font-bold">Recently Watched</p>
+                <hr className="w-[75%]" />
+            </div>
+            <div className="flex justify-center items-center h-[120px] border border-white/15">
+              <p className="opacity-80">Add films to your watched list.</p>
+            </div>
           </div>
-          <button
-            onClick={scrollRight}
-            className="absolute hidden carouselArrow top-0 bottom-0 right-0 z-10 bg-transparent hover:bg-black/50 transition-colors duration-200 cursor-pointer"
-          >
-            <ChevronRight />
-          </button>
+          <div className="flex flex-col gap-4">
+            <div>
+              <p className="text-lg font-bold">Trending Right Now</p>
+              <hr className="w-[75%]" />
+            </div>
+            <div className="relative">
+              <button
+                onClick={scrollLeft}
+                className="absolute carouselArrow top-0 bottom-0 left-0 z-10 opacity-0 bg-transparent active:bg-black/50 transition-colors duration-500 cursor-pointer"
+              >
+                <ChevronLeft size={80}/>
+              </button>
+              <div
+                className="flex gap-3 overflow-x-auto no-scrollbar snap-x snap-mandatory"
+                ref={carouselRef}
+              >
+                {trendingMovies.map((movie) => {
+                  return (
+                    <div key={movie.id} className="shrink-0 snap-start">
+                      <img
+                        src={movie.poster}
+                        alt={movie.title}
+                        className="w-[9rem] md:w-[10rem] lg:w-[11rem] aspect-[2/3] border border-white/15 object-cover"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+              <button
+                onClick={scrollRight}
+                className="absolute carouselArrow top-0 bottom-0 right-0 z-10 opacity-0 bg-transparent active:bg-black/50 transition-colors duration-500 cursor-pointer"
+              >
+                <ChevronRight size={80}/>
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div>
+                <p className="text-lg font-bold">What Your Friends Are Watching</p>
+                <hr className="w-[75%]" />
+            </div>
+            <div className="flex justify-center items-center h-[120px] border border-white/15">
+              <p className="opacity-80">Add friends to see what they're watching.</p>
+            </div>
         </div>
       </div>
     </main>
--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,14 +1,35 @@
 import FourthIcon from "../../assets/fourthicon.png"
+import { useState, useRef, useEffect } from "react"
+import { useClickOutside } from "../../hooks/clickOutsideDiv.js";
 
 export default function MainHeader(){
+
+    const [dropDownOpen, setDropDownOpen] = useState(false)
+    const dropDownRef = useRef(null);
+        
+    function handleDropDown(){
+        setDropDownOpen((current) => !current)
+    }
+
+    function closeDropDown(){
+        setDropDownOpen(false)
+    }
+
+    useClickOutside(dropDownRef, closeDropDown)
+
     return (
         <header className="flex justify-between py-3 px-2 shadow-[var(--shadow-bottom)]">
             <div className="flex gap-2 items-center">
                 <img src={FourthIcon} alt="Fourth Logo" className="w-[3rem] h-[3rem]"/>
                 <p className="text-xl font-bold">Home</p>
             </div>
-            <div className="flex items-center justify-center border border-2 border-[var(--accent-dark)] px-2 py-2 rounded-full">
-                <img src={FourthIcon} alt="User's account information." className="w-[2rem] h-[2rem]"/>
+            <div ref={dropDownRef} className="relative flex items-center justify-center border-2 border-[var(--accent-dark)] px-2 py-2 rounded-full cursor-pointer">
+                <button onClick={handleDropDown}>
+                    <img src={FourthIcon} alt="User's account information." className="w-[2rem] h-[2rem] cursor-pointer"/>
+                </button>
+                { dropDownOpen && <div className="fixed sm:absolute py-4 px-2 sm:right-full bg-[var(--surface)] top-0 sm:top-14 left-0 sm:left-auto w-[100vw] sm:w-[400px] h-[100dvh] sm:h-[500px] z-20000 rounded-lg">
+                    <p>Log Out</p>
+                </div> }
             </div>
         </header>
     )
