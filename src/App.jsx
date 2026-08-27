fd04c8064f2d88c0b1654461cf87092fafb77921
Nehemiah Brown <nbrowndeveloper@gmail.com>
2026-08-26 19:22:52 -0500

    setting up currentUser auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from "./services/firebase.js"
 import { getAPI } from "./services/tmdb.js";
 
 import SignUp from "./components/auth/SignUp";
@@ -11,6 +13,19 @@ import PublicLayout from "./layouts/PublicLayout";
 import Home from "./components/common/Home";
 
 export default function App() {
+
+  onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,11 +1,18 @@
-import FourthIcon from "../../assets/fourthicon.png"
-import { useState, useRef, useEffect } from "react"
+import { getAuth, signOut } from "firebase/auth";
+import { auth } from "../../services/firebase.js";
+
+import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router";
+
 import { useClickOutside } from "../../hooks/clickOutsideDiv.js";
+import FourthIcon from "../../assets/fourthicon.png"
+
 
 export default function MainHeader(){
 
     const [dropDownOpen, setDropDownOpen] = useState(false)
     const dropDownRef = useRef(null);
+    const navigate = useNavigate()
         
     function handleDropDown(){
         setDropDownOpen((current) => !current)
@@ -17,6 +24,14 @@ export default function MainHeader(){
 
     useClickOutside(dropDownRef, closeDropDown)
 
+    function signUserOut(){
+        signOut(auth).then(() => {
+            navigate("/")
+          }).catch((error) => {
+            console.log(error.message)
+          });
+    }
+
     return (
         <header className="flex justify-between py-3 px-2 shadow-[var(--shadow-bottom)]">
             <div className="flex gap-2 items-center">
@@ -27,8 +42,8 @@ export default function MainHeader(){
                 <button onClick={handleDropDown}>
                     <img src={FourthIcon} alt="User's account information." className="w-[2rem] h-[2rem] cursor-pointer"/>
                 </button>
-                { dropDownOpen && <div className="fixed sm:absolute py-4 px-2 sm:right-full bg-[var(--surface)] top-0 sm:top-14 left-0 sm:left-auto w-[100vw] sm:w-[400px] h-[100dvh] sm:h-[500px] z-20000 rounded-lg">
-                    <p>Log Out</p>
+                { dropDownOpen && <div className="fixed sm:absolute sm:right-full bg-[var(--surface)] top-0 sm:top-14 left-0 sm:left-auto w-[100vw] sm:w-[400px] h-[100dvh] sm:h-[500px] z-20000 rounded-lg">
+                    <button onClick={signUserOut} className="border-b border-white/15 w-full text-left py-4 px-2 text-lg hover:bg-[var(--accent-dark)] hover:text-[var(--primary-text)] active:scale-98 hover:rounded-t-lg transition-all duration-200 cursor-pointer">Log Out</button>
                 </div> }
             </div>
         </header>
